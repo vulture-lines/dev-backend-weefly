e7072b48e9285c2dd6937537d7c94cd7b7013984
Hariraghav1026 <hariraghav@notasco.com>
2025-06-25 11:00:09 +0530

    Select Flight Booking API check-25-06-2025

--- controller/travelfusion/routes/Travelfusionapi.js
+++ controller/travelfusion/routes/Travelfusionapi.js
@@ -1,9 +1,10 @@
 // routes/routing.js
 const express = require("express");
 const router = express.Router();
-const { startRouting ,checkRouting} = require("../controller/Startrouting");
+const { startRouting ,checkRouting,processDetails} = require("../controller/Startrouting");
 
 // POST /api/start-routing
 router.post("/start-routing", startRouting);
 router.post('/check-routing', checkRouting);
+router.post('/process-details', processDetails);
 module.exports = router;
