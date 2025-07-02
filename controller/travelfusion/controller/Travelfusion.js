afd856ca5fd944472fdaf73df1a3a639d1f43c06
Hariraghav1026 <hariraghav@notasco.com>
2025-07-02 19:43:10 +0530

    Booking API debug-02-07-2025

--- controller/travelfusion/controller/Travelfusion.js
+++ controller/travelfusion/controller/Travelfusion.js
@@ -341,7 +341,7 @@ const startBooking = async (req, res) => {
   try {
     const {
       expectedAmount,
-      expectedCurrency = "GBP",
+      expectedCurrency,
       TFBookingReference,
       fakeBooking = true,
     } = req.body;
@@ -380,7 +380,7 @@ const startBooking = async (req, res) => {
       },
       timeout: 120000,
     });
-    //  return res.status(200).send(response.data);
+     return res.status(200).send(response.data);
 
     const parsed = await parseStringPromise(response.data);
     const result = parsed?.CommandList?.StartBooking?.[0];
