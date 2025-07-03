2676855b48054015ac24c2962b71ce5ca4ce24ae
Hariraghav1026 <hariraghav@notasco.com>
2025-07-03 11:14:51 +0530

    Check routing debug-03-07-2025

--- controller/travelfusion/controller/Travelfusion.js
+++ controller/travelfusion/controller/Travelfusion.js
@@ -176,7 +176,7 @@ const checkRouting = async (req, res) => {
     const parsed = await parseStringPromise(response.data);
   
     const checkRoutingResponse = parsed?.CommandList?.CheckRouting?.[0];
-      // return res.status(200).json({checkRoutingResponse});
+      return res.status(200).json({checkRoutingResponse});
     const routeId = checkRoutingResponse?.RoutingId;
     const flightList = checkRoutingResponse?.RouterList;
     res.status(200).json({ routingId: routeId, flightList: flightList });
