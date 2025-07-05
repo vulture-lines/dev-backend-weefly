54ef70c36da8f446fcde26851ddd850aa923baae
Hariraghav1026 <hariraghav@notasco.com>
2025-07-05 16:24:34 +0530

    XML API log-05-07-2025

--- controller/travelfusion/controller/Travelfusion.js
+++ controller/travelfusion/controller/Travelfusion.js
@@ -126,14 +126,13 @@ const startRouting = async (req, res) => {
     );
     const parsed = await parseStringPromise(response.data);
     const startRoutingResponse = parsed?.CommandList?.StartRouting?.[0];
-    res.status(200).send(routingXml);
     if (!startRoutingResponse?.RoutingId?.[0]) {
       return res.status(422).json({
         error: "No RoutingId returned",
         requestdata: response.data,
       });
     }
-    // return res.status(422).send(routingXml);
+    return res.status(422).send(response.data);
     res.status(200).json({
       routingId: startRoutingResponse.RoutingId[0],
       // routerList: startRoutingResponse.RouterList || [],
