65a503702de30914918d2a1408e2e0a19cec8415
Hariraghav1026 <hariraghav@notasco.com>
2025-07-07 20:16:09 +0530

    Hotel API-07-07-2025

--- api/travelfusion/hotel/controller/Travelfusionhotel.js
+++ api/travelfusion/hotel/controller/Travelfusionhotel.js
@@ -14,7 +14,7 @@ const startRoutingHotel = async (req, res) => {
       checkinDate,
       checkoutDate,
       accommodationTypes = [],
-      // supplierList = [],
+      supplierList = [],
       timeout = 40,
       rooms = [],
     } = req.body;
@@ -44,9 +44,9 @@ const startRoutingHotel = async (req, res) => {
           CheckoutDates: {
             DateOfSearch: checkoutDate,
           },
-          // SupplierList: {
-          //   Supplier: supplierList,
-          // },
+          SupplierList: {
+            Supplier: supplierList,
+          },
           Timeout: timeout,
           AccommodationTypeList: {
             AccommodationType: accommodationTypes,
