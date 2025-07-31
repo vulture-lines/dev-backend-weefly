628d4aeaeb9c5a2c69fa04ecf01c4e84740c5d77
Hariraghav.S <hariraghava21s@gmail.com>
2025-07-31 22:44:57 +0530

    TF Update-31-07-2025

--- controller/Commissioncontroller.js
+++ controller/Commissioncontroller.js
@@ -1,19 +1,21 @@
 // controllers/commissionController.js
 
 const commissionDetails = require("../models/Commission");
-
+const { Payment } = require("../models/SISSPPaymentdb");
 exports.addCommissionAndTax = async (req, res) => {
   try {
-    const { commission, tax , type } = req.body;
+    const { commission, tax, type } = req.body;
 
     if (commission == null || tax == null || type == "") {
-      return res.status(400).json({ message: "Commission and tax are required" });
+      return res
+        .status(400)
+        .json({ message: "Commission and tax are required" });
     }
 
     const newEntry = new commissionDetails({
       Commission: commission,
       Tax: tax,
-      CommissionType:type
+      CommissionType: type,
     });
 
     await newEntry.save();
@@ -25,13 +27,33 @@ exports.addCommissionAndTax = async (req, res) => {
   }
 };
 
+exports.getDetails = async (req, res) => {
+  const commissionDetail = await commissionDetails.find();
+  res.status(200).json({ commissionDetail });
+};
 
-exports.getDetails=async (req,res)=>{
-    const commissionDetail=await commissionDetails.find();
-    res.status(200).json({commissionDetail})
-} 
+exports.getCommisionDetails = async (req, res) => {
+  const commissionDetail = await commissionDetails.findOne().sort({ _id: -1 });
+  res.status(200).json({ commissionDetail });
+};
 
-exports.getCommisionDetails=async (req,res)=>{
-    const commissionDetail=await commissionDetails.findOne().sort({_id:-1});
-    res.status(200).json({commissionDetail})
-}   
+exports.getTicketDetail = async (req, res) => {
+  const ticketId = req.params.ticketId;
+  try {
+    const doc = await Payment.findOne({
+      "TravelfusionBookingDetails.BookingCheckResponse.additionalInfo.SupplierReference":
+        ticketId,
+    });
+    console.log(doc);
+
+    if (doc) {
+      return res.status(200).json({
+        details:doc
+      });
+    } else {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
--- routes/Commisionroutes.js
+++ routes/Commisionroutes.js
@@ -4,4 +4,5 @@ const commissionController = require("../controller/Commissioncontroller");
 router.get("/getcommissiondetails", commissionController.getCommisionDetails);
 router.get("/getdetails", commissionController.getDetails);
 router.post("/addcommissiondetail", commissionController.addCommissionAndTax);
+router.get("/getticketdetail/:ticketId",commissionController.getTicketDetail);
 module.exports = router;
