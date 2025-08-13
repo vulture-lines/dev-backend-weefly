efdb165bdb2cd0cda429879e10b5c763a93dd85f
Hariraghav.S <hariraghava21s@gmail.com>
2025-08-13 13:32:03 +0530

    Fix-13-08-2025

--- api/travelfusion/flight/controller/Travelfusionflight.js
+++ api/travelfusion/flight/controller/Travelfusionflight.js
@@ -441,9 +441,9 @@ const processTerms = async (req, res) => {
         csps = [csps];
       }
 
-      // if (seat) {
-      //   csps.push({ Name: "SeatOptions", Value: `${seat}` });
-      // }
+      if (seat) {
+        csps.push({ Name: "SeatOptions", Value: `${seat}` });
+      }
 
       if (luggage) {
         csps.push({ Name: "LuggageOptions", Value: luggage });
@@ -500,13 +500,7 @@ const processTerms = async (req, res) => {
         Value: "Always",
       });
     }
-    // Add seatOptions globally
-    if (seatOptions.length > 0) {
-      globalCSPs.push({
-        Name: "SeatOptions",
-        Value: seatOptions.join(","),
-      });
-    }
+
     // ✅ Rebuild bookingProfileObj with CSPs first
     const bookingProfileObj = {
       CustomSupplierParameterList: {
@@ -1086,7 +1080,7 @@ const createVirtualCard = async (req, res) => {
       amount,
       currency,
       expirationDate, // format: "dd/mm/yyyy"
-      locator,
+      locator
     } = req.body;
 
     // Check for required inputs
@@ -1149,5 +1143,5 @@ module.exports = {
   getCurrencyList,
   getAirports,
   getSupplierRoutes,
-  createVirtualCard,
+  createVirtualCard
 };
